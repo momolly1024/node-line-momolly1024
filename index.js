6bd8bfeb66fb22ca11a30aaa7836a25c1fccd96b
momolly1024 <61406208+momolly1024@users.noreply.github.com>
2020-04-28 15:33:34 +0800

    先丟上去
    LINE_BOT

---
+++ index.js
@@ -0,0 +1,30 @@
+// 引用linebot套件
+import linebot from 'linebot'
+// 引用 dotenv套件
+import dotenv from 'dotenv'
+//
+import rp from 'request-promise'
+// 讀取 .env檔
+dotenv.config()
+// 宣告機器人的資訊
+const bot = linebot({
+  channelId: process.env.CHANNEL_ID,
+  channelSecret: process.env.CHANNEL_SECRET,
+  channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN
+})
+// 當收到訊息時
+bot.on('message', async (event) => {
+  let msg = ''
+  try {
+    const data = await rp({ uri: 'https://kktix.com/events.json', json: true })
+    msg = data.entry[0].title
+  } catch (error) {
+    msg = '發生錯誤'
+  }
+  event.reply(msg)
+})
+
+// 在 port 啟動
+bot.listen('/', process.env.PORT, () => {
+  console.log('機器人已啟動')
+})
