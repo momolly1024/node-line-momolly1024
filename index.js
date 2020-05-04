d54579e03155d5a9025a4afae80e70c76d178c4b
momolly1024 <momolly1024@gmail.com>
2020-05-04 11:21:49 +0800

    a

--- index.js
+++ index.js
@@ -16,8 +16,8 @@ const bot = linebot({
 bot.on('message', async (event) => {
   let msg = ''
   try {
-    const data = await rp({ uri: 'https://kktix.com/events.json', json: true })
-    msg = data.entry[0].title
+    const data = await rp({ uri: 'https://data.taipei/opendata/datalist/apiAccess?scope=resourceAquire&rid=a3e2b221-75e0-45c1-8f97-75acbd43d613', json: true })
+    msg = data.result.results.A_Pic04_ALT[2]
     console.log(msg)
   } catch (error) {
     msg = '發生錯誤'
