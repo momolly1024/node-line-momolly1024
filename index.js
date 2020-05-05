d7cb66df57c0a39ef12c0b496925efa48cecb03f
momolly1024 <momolly1024@gmail.com>
2020-05-05 09:00:56 +0800

    新增判斷2

--- index.js
+++ index.js
@@ -26,26 +26,33 @@ bot.on('message', async (event) => {
       for (let i = 0; i < data.results.length; i++) {
         msg += data.results[i].title + '\n'
       }
-    } else if (event.message.type === 'image') {
-      for (let i = 0; i < data.results.length; i++) {
-        msg = {
-          type: 'location',
-          title: 'my location',
-          address: '〒150-0002 東京都渋谷区渋谷２丁目２１−１',
-          latitude: 35.65910807942215,
-          longitude: 139.70372892916203
-        }
+    } else if (event.message.text === '機器人在哪') {
+      msg = {
+        type: 'location',
+        title: '就是那個雲端，使用TMDB抓資料',
+        address: '25172新北市淡水區中正路一段2號1樓(國賓影城@淡水禮萊廣場)',
+        latitude: 25.176703,
+        longitude: 121.429059
       }
     } else if (event.message.type === 'sticker') {
-      msg = {
+      msg = [{
+        type: 'text',
+        text: '不管怎樣，送你一個抱抱啦'
+      }, {
         type: 'sticker',
         packageId: '11537',
         stickerId: '52002737'
-      }
+      }]
     } else if (event.message.type === 'image') {
       msg = '抱歉，我不能收你的照片:<'
+    } else if (event.message.text === '機器人長相') {
+      msg = {
+        type: 'image',
+        originalContentUrl: 'https://i.imgur.com/ZTxlAke.png',
+        previewImageUrl: 'https://i.imgur.com/ZTxlAke.png'
+      }
     } else {
-      msg = '請輸入【now_playing】、【熱映中】或指定熱播排名(1~20)，查看近期熱播中的電影！'
+      msg = '請輸入【now_playing】、【熱映中】或指定熱播排名(1~20)，查看近期熱播中的電影！\n 好啦還是你想知道...【機器人在哪】、【機器人長相】、【???】'
     }
   } catch (error) {
     msg = '發生錯誤'
