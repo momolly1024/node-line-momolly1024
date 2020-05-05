12476490c2a5c16cd2e2ce3908d8f0b49f676172
momolly1024 <momolly1024@gmail.com>
2020-05-05 08:30:59 +0800

    新增判斷

--- index.js
+++ index.js
@@ -22,6 +22,20 @@ bot.on('message', async (event) => {
         // movie += data.result[i].original_title
         msg += data.results[i].original_title + '\n'
       }
+    } else if (event.message.text === '熱映中') {
+      for (let i = 0; i < data.results.length; i++) {
+        msg += data.results[i].title + '\n'
+      }
+    } else if (event.message.type === 'image') {
+      for (let i = 0; i < data.results.length; i++) {
+        msg = {
+          type: 'location',
+          title: 'my location',
+          address: '〒150-0002 東京都渋谷区渋谷２丁目２１−１',
+          latitude: 35.65910807942215,
+          longitude: 139.70372892916203
+        }
+      }
     } else if (event.message.type === 'sticker') {
       msg = {
         type: 'sticker',
@@ -31,7 +45,7 @@ bot.on('message', async (event) => {
     } else if (event.message.type === 'image') {
       msg = '抱歉，我不能收你的照片:<'
     } else {
-      msg = '請輸入now_playing'
+      msg = '請輸入【now_playing】、【熱映中】或指定熱播排名(1~20)，查看近期熱播中的電影！'
     }
   } catch (error) {
     msg = '發生錯誤'
